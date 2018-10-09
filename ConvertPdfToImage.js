f4e672bde7fbcd959c46c539d0a27274d0ee28bd
myh999 <myh999@myh999ThinkpadCarbon.lan>
2018-10-09 19:59:02 -0400

    Use Cloud Vision OCR to detect document

--- ConvertPdfToImage.js
+++ ConvertPdfToImage.js
@@ -1,11 +1,42 @@
-var PDFImage = require("pdf-image").PDFImage;
+const vision = require('@google-cloud/vision').v1;
 
-var pdfImage = new PDFImage("SampleImages/Image1.pdf", {
-    convertOptions: {
-        "-quality": "100"
-    }
-});
-pdfImage.convertPage(0).then(function(imagePath) {
-    fs.existsSync("SampleImages/Image1-0.png");
-    console.log("Conversion Successful");
+const client = new vision.ImageAnnotatorClient();
+
+const bucketName = 'myh999-bucket1';
+const fileName = 'workout-sheet-converter/sample_images/Image1.pdf';
+const gcsSourceUri = 'gs://myh999-bucket1/workout-sheet-converter/sample_images/Image1.pdf';
+const gcsDestinationUri = 'gs://myh999-bucket1/workout-sheet-converter/sample_images/Image1.json';
+
+const inputConfig = {
+    mimeType: 'application/pdf',
+    gcsSource: {
+        uri: gcsSourceUri,
+    },
+};
+
+const outputConfig = {
+    gcsDestination: {
+        uri: gcsDestinationUri,
+    },
+};
+
+const features = [{type: 'DOCUMENT_TEXT_DETECTION'}];
+const request = {
+    requests: [
+        {
+            inputConfig: inputConfig,
+            outputConfig: outputConfig,
+            features: features,
+        },
+    ],
+};
+
+client.asyncBatchAnnotateFiles(request).then(results => {
+    const operation = results[0];
+    return operation.promise();
+}).then(filesResponse => {
+    const destinationUri = filesResponse[0].responses[0].outputConfig.gcsDestination.uri;
+    console.log('Json saved to: ' + destinationUri);
+}).catch(err => {
+    console.error('ERROR: ', err);
 });
