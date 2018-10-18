a265a4457a6215decec7846674921642fee56bb1
myh999 <myh999@myh999ThinkpadCarbon.lan>
2018-10-17 20:52:13 -0400

    Add text.split to GenerateExerciseList

--- scripts/GenerateExerciseList.js
+++ scripts/GenerateExerciseList.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
 const inputPath = '../SampleOutput/Image1jpgText.txt'
 const outputPath = '../SampleOutput/Image1ExerciseList.txt'
-
+var text = fs.readFileSync(inputPath).toString('utf-8');
+var textSplit = text.split(' ');
+console.log(textSplit);
