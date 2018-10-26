0b3deb53d680ce476c41b423176dbdd89b2c0e19
myh999 <myh999@myh999ThinkpadCarbon.lan>
2018-10-25 21:16:34 -0400

    Complete implementation of mergedArray

--- scripts/CoordinatesHelper.js
+++ scripts/CoordinatesHelper.js
@@ -10,8 +10,89 @@ function getYMax(data) {
     return Math.max.apply(null, yArray);
 }
 
+function invertAxis(data, yMax) {
+    data = fillMissingValues(data);
+    for (let i = 1; i < data.textAnnotations.length; i++) {
+        let vertices = data.textAnnotations[i].boundingPoly.vertices;
+        for (let j = 0; j < 4; j++) {
+            vertices[j]['y'] = (yMax - vertices[j]['y']);
+        }
+    }
+    return data;
+}
+
+function fillMissingValues(data) {
+    for (let i = 1; i < data.textAnnotations.length; i++) {
+        let vertices = data.textAnnotations[i].boundingPoly.vertices;
+        vertices.map((vertex) => {
+            if (vertex['x'] === undefined) {
+                vertex['x'] = 0;
+            }
+            if (vertex['y'] === undefined) {
+                vertex['y'] = 0;
+            }
+        });
+    }
+    return data;
+}
+
+function getBoundingPolygon(mergedArray) {
+    for (let i = 0; i < mergedArray.length; i++) {
+        let array = [];
+
+        let height1 = mergedArray[i].boundingPoly.vertices[0].y - mergedArray[i].boundingPoly.vertices[3].y;
+        let height2 = mergedArray[i].boundingPoly.vertices[1].y - mergedArray[i].boundingPoly.vertices[2].y;
+        let height = height1;
+        if (height2 > height1) {
+            height = height2;
+        }
+        let avgHeight = h * 0.6;
+        
+        array.push(mergedArray[i].boundingPoly.vertices[1]);
+        array.push(mergedArray[i].boundingPoly.vertices[0]);
+    }
+}
+
+function getRectangle(vertices, isRoundValues, avgHeight, isAdd) {
+    if (isAdd) {
+        v[1].y += avgHeight;
+        v[0].y += avgHeight;
+    } else {
+        v[1].y -= avgHeight;
+        v[0].y -= avgHeight;
+    }
+
+    let yDiff = (v[1].y - v[0].y);
+    let xDiff = (v[1].x - v[0].x);
+
+    let gradient = yDiff / xDiff;
+
+    let xThreshMin = 1;
+    let xThreshMax = 2000;
+
+    let yMin;
+    let yMax;
+    if (gradient === 0) {
+        yMin = v[0].y;
+        yMax = v[0].y;
+    } else {
+        yMin = (v[0].y) - (gradient * (v[0].x - xThreshMin));
+        yMax = (v[0].y) + (gradient * (xThreshMax - v[0].x));
+    }
+
+    if (isRoundValues) {
+        yMin = Math.round(yMin);
+        yMax = Math.round(yMax);
+    }
+    return {xMin: xThreshMin, xMax: xThreshMax, yMin: yMin, yMax: yMax};
+}
+
 var exports = module.exports = {};
 
 exports.getYMax = function(data) {
     return getYMax(data);
+};
+
+exports.invertAxis = function(data, yMax) {
+    return invertAxis(data, yMax);
 };
--- scripts/ProcessJson.js
+++ scripts/ProcessJson.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const deepcopy = require('deepcopy');
+const path = require('path');
 const coordinatesHelper = require('./CoordinatesHelper');
 
-const filePath = '../SampleOutput/Image1jpgResults3.json';
+const appDir = path.dirname(require.main.filename);
+const filePath = appDir + '/../SampleOutput/Image1jpgResults3.json';
 
 const content = fs.readFileSync(filePath);
 const textJson = JSON.parse(content);
@@ -11,6 +13,51 @@ initLineSegmentation(textJson);
 
 function initLineSegmentation(data) {
     const yMax = coordinatesHelper.getYMax(data);
-    console.log("YMax: " + yMax);
-    
+    data = coordinatesHelper.invertAxis(data, yMax);
+
+    let lines = data.textAnnotations[0].description.split('\n');
+    let rawText = deepcopy(data.textAnnotations);
+
+    lines = lines.reverse();
+    rawText = rawText.reverse();
+    rawText.pop();
+
+    let mergedArray = getMergedLines(lines, rawText);
+
+}
+
+function getMergedLines(lines, rawText) {
+    let mergedArray = [];
+    while (lines.length !== 1) {
+        let line = lines.pop();
+        let lineCopy = deepcopy(line);
+        let isStart = true;
+
+        let data = "";
+        let mergedElement;
+        let wordElement;
+
+        do {
+            wordElement = rawText.pop();
+            if (wordElement !== undefined) {
+                let word = wordElement.description;
+                let index = line.indexOf(word);
+                line = line.substring(index + word.length);
+                if (isStart) {
+                    isStart = false;
+                    // Get coordinates of first word
+                    mergedElement = wordElement;
+                }
+            }
+        } while (wordElement !== undefined && line !== "");
+
+        // Get coordinates of last word
+        if (line === "") {
+            mergedElement.description = lineCopy;
+            mergedElement.boundingPoly.vertices[1] = wordElement.boundingPoly.vertices[1];
+            mergedElement.boundingPoly.vertices[2] = wordElement.boundingPoly.vertices[2];
+            mergedArray.push(mergedElement);
+        }
+    }
+    return mergedArray;
 }
